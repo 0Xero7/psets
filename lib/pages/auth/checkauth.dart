6a7e824142e15dff1a0a469f0848f60413c573d3
Soumya Pattanayak <smpsmp13@gmail.com>
2020-06-22 16:13:39 +0530

    added under maintainance page

--- lib/pages/auth/checkauth.dart
+++ lib/pages/auth/checkauth.dart
@@ -1,10 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:psettracker/static/authservice.dart';
 import 'package:psettracker/widgets/pagewrapper.dart';
 
-class CheckAuth extends StatelessWidget {
+class CheckAuth extends StatefulWidget {
 
+  @override
+  _CheckAuthState createState() => _CheckAuthState();
+}
+
+class _CheckAuthState extends State<CheckAuth> with SingleTickerProviderStateMixin {
   void _checkAuth(context) async {
     // wait for the Auth object to be initialized
     await Future.delayed(Duration(seconds: 2));
@@ -14,13 +21,69 @@ class CheckAuth extends StatelessWidget {
     if (!_signedIn) Navigator.popAndPushNamed(context, '/login/loginpage');
     else {
       Navigator.popAndPushNamed(context, '/load');
-    }
-      
+    }      
   }
 
+  Future<bool> _checkReady(context) async {
+    var res = await Firestore.instance.document('/stats/dev').get();
+    if ((res.data['under_construction'] as bool) == true) return false;
+    return true;
+  }
+
+  AnimationController _controller;
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    )..repeat();
+  }
+
+  
   @override
   Widget build(BuildContext context) {
-    _checkAuth(context);
-    return PageWrapper(child: Container());
+    return StreamBuilder(
+      stream: Firestore.instance.document('/stats/dev').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          DocumentSnapshot res = snapshot.data;
+          if ((res.data['under_maintainance'] as bool) == false) {
+            _checkAuth(context);
+          } else {
+            return PageWrapper(
+              child: Center(
+                child: Container(
+                  width: 250,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+
+                    children: [
+                      Transform.rotate(
+                        angle: 0,
+                        child: Icon(Icons.settings, size: 100, color: Colors.grey),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        'Under Maintainance.',
+                        style: GoogleFonts.nunito(fontSize: 20),
+                      ),
+                      const SizedBox(height:5),
+                      Text(
+                        'Please try again later, or wait here to be redirected automatically once maintainance is complete.',
+                        style: GoogleFonts.nunito(fontSize: 14, color: Colors.grey),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                )
+              ),
+            );
+          }
+        }
+        
+        return PageWrapper(child: Container());
+      }
+    );
   }
 }
